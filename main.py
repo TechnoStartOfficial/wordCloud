199e6f6b82aee58d9ca9bf165163d4a55d9928b6
TechnoStartOfficial <rambpandey238@gmail.com>
2022-04-24 09:25:34 +0530

    wordCloud : Added Initial text_count

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ def filter_string(inputString):
     for i in unwanted:
         outputString=outputString.replace(i,"")
     return outputString
+def text_count(text):
+    result={}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return result
