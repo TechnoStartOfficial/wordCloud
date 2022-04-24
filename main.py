cd404b0abf1ccc3670b5e9c7c4f1ba03247bff28
TechnoStartOfficial <rambpandey238@gmail.com>
2022-04-24 09:34:27 +0530

    wordCloud: Improved unwanted word filter

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def filter_string(inputString):
     for i in inputString:
         if inputString.isalnum():
             outputString+=i
-    unwanted = ['if','but','why','no','hence','although']
+    unwanted = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
     for i in unwanted:
         outputString=outputString.replace(i,"")
     return outputString
