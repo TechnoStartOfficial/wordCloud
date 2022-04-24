b0b30d41f6091010000f6fbda78e229ff08ca12b
TechnoStartOfficial <rambpandey238@gmail.com>
2022-04-24 16:11:14 +0530

    wordCloud: redesign format_string function

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 def filter_string(inputString):
-    outputString = ""
+    cleanString=""
     for i in inputString:
-        if inputString.isalnum():
-            outputString+=i
-    unwanted = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
-    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
-    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
-    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
-    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
-    for i in unwanted:
-        outputString=outputString.replace(i,"")
-    return outputString
+        if i.isalnum() or i == " ":
+            cleanString+=i
+    cleanList = cleanString.split()
+    unwantedChars = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+        "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+        "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+        "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+        "all", "any", "both" ," ", "1","2","3","4","5","6","7","8","9", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    for i in unwantedChars:
+        for j in range(0,len(cleanList)):
+            if i == cleanList[j]:
+                cleanList[j]=""
+    return cleanList
 def text_count(text):
     result={}
     for letter in text:
@@ -21,3 +24,5 @@ def text_count(text):
         result[letter] += 1
     return result
 f = open("text.txt","r").read()
+
+
